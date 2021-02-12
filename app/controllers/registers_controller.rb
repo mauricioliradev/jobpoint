0428422805728dc2c0c0472d23f28d52c11a7b58
mauricioliradev <mauricioliraweb@gmail.com>
2021-02-12 00:53:53 -0300

    add jquery, style form devise and i18n

---
+++ app/controllers/registers_controller.rb
@@ -0,0 +1,69 @@
+class RegistersController < ApplicationController
+  before_action :set_register, only: [:show, :edit, :update, :destroy]
+
+  # GET /registers
+  def index
+    @registers = Register.all
+  end
+
+  # GET /registers/1
+  def show
+  end
+
+  # GET /registers/new
+  def new
+    @register = Register.new
+  end
+
+  # GET /registers/1/edit
+  def edit
+  end
+
+  # POST /registers
+  def create
+    @register = Register.new(register_params)
+
+    respond_to do |format|
+      if @register.save
+        format.html { redirect_to @register, notice: t('controllers.registers.created') }
+        format.json { render :show, status: :created, location: @register }
+      else
+        format.html { render :new }
+        format.json { render json: @register.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+    # PATCH/PUT /registers/1
+  def update
+    respond_to do |format|
+      if @register.update(register_params)
+        format.html { redirect_to @register, notice: t('controllers.registers.updated') }
+        format.json { render :show, status: :ok, location: @register }
+      else
+        format.html { render :edit }
+        format.json { render json: @register.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /registers/1
+  def destroy
+    @register.destroy
+    respond_to do |format|
+      format.html { redirect_to registers_url, notice: t('controllers.registers.destroyed') }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_register
+      @register = Register.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def register_params
+      params.require(:register).permit(:user_id, :kind, :date, :description)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
   devise_for :users,
   controllers: {
     registrations: 'users/registrations'
+  },
+  path_names: {
+    sign_out:     :sair,
+    sign_in:      :login,
+    password:     :secret,
+    confirmation: :verification,
+    unlock:       :unblock,
+    registration: :'minha-conta',
+    sign_up:      :'cadastre-se',
+    edit:         :'editar-perfil'
   }
+  resources :registers
+  root 'pages#home'
 end
